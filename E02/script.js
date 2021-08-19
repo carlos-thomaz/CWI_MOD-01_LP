5c366d9a46f4e21a11accf56268ca43ba0a56787
Carlos Thomaz <carlitos.astrada@gmail.com>
2021-08-19 01:20:52 -0300

    JS commit

---
+++ E02/script.js
@@ -0,0 +1,11 @@
+var nomeCompleto = "Carlos Thomaz da Rosa Silva"
+var apelido = "Carlitos"
+var idade = 33
+var dataNascimento = "02/02/1988"
+var localNascimento = "Porto Alegre"
+var altura = 1.90
+var trabalhando = false
+
+var apresentacao = "Meu nome é " + nomeCompleto + " (sou conhecido[a] como " + apelido + ") e tenho " + idade + " anos. Nasci no dia " + dataNascimento + ", na cidade de " + localNascimento + ". Tenho " + altura + "m de altura e atualmente estou " + (trabalhando ? "empregado" : "desempregado") + "."
+
+console.log(apresentacao)
